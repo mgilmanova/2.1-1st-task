17d53544b3d6f92c73123fd5ca2c0761eceaafe7
Marina Gilmanova <gmarrin@mail.ru>
2021-01-05 16:46:19 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price;
+        price = 15000;
+        int bonus;
+        bonus = price / 20;
+        System.out.println(bonus);
+    }
+
+}
